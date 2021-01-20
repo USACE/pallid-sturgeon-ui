80bb405ae472cba7b2993d2dfc325bcc9e98d586
593551 <mohitbisht@c02dl0zqmd6v.home>
2021-01-20 01:27:24 -0500

    install and configure redux and react router

---
+++ src/app/store/store.js
@@ -0,0 +1,34 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'connected-react-router';
+import thunk from 'redux-thunk';
+import { createBrowserHistory } from 'history';
+import rootReducer from './reducers';
+
+export const history = createBrowserHistory();
+const initialState = {};
+const enhancers = [];
+const middleware = [
+  thunk,
+  routerMiddleware(history),
+];
+
+// only enable redux devtools when running in development mode
+if (process.env.REACT_APP_DEVTOOLS === 'true') {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers,
+);
+
+const store = createStore(
+  rootReducer(history),
+  initialState,
+  composedEnhancers,
+);
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import App from './app/App';
+import store, { history } from './app/store/store';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ConnectedRouter>
+  </Provider>,
+  rootElement,
 );
 
 // If you want to start measuring performance in your app, pass a function
