e4751f146bd39ab2d20a04c41e12fddd5a1d6748
KevinJJackson <kevinjjackson93@gmail.com>
2021-07-13 15:16:36 -0400

    breadcrumb adjustments

--- src/app-components/page-content/pageContent.js
+++ src/app-components/page-content/pageContent.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import { connect } from 'redux-bundler-react';
 
+import Breadcrumb from 'app-components/breadcrumb';
 import { classArray } from '../../utils';
 
 import './pageContent.scss';
 
 const hasDevBanner = process.env.REACT_APP_DEVELOPMENT_BANNER;
 
-const PageContent = ({ children }) => {
-  const pageClasses = classArray([
-    hasDevBanner && 'banner',
-  ]);
+const PageContent = connect(
+  'selectPathname',
+  ({ pathname, children }) => {
+    const pageClasses = classArray([
+      hasDevBanner && 'banner',
+    ]);
 
-  return (
-    <div className={pageClasses}>
-      {children}
-    </div>
-  );
-};
+    const hasBreadcrumb = pathname != '/';
+
+    return (
+      <div className={pageClasses}>
+        {hasBreadcrumb && (
+          <Breadcrumb pathname={pathname} />
+        )}
+        {children}
+      </div>
+    );
+  }
+);
 
 export default PageContent;
