5bea70b153e507589f567620e870d391596add23
Kevin Jackson <30411845+KevinJJackson@users.noreply.github.com>
2021-08-02 14:40:44 -0400

    enhancment/cleanup-pages (#6)
    * export as csv, margins, styles
    * padding adjustments

--- src/app-pages/data-summaries/tables/fishTable.js
+++ src/app-pages/data-summaries/tables/fishTable.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { AgGridReact, AgGridColumn } from 'ag-grid-react';
 
+import DownloadAsCSV from '../components/downloadAsCSV';
+
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 const FishTable = () => (
-  <div className='ag-theme-balham' style={{ width: '100%', height: '600px' }}>
-    <AgGridReact rowData={[]}>
-      <AgGridColumn field='Unique ID' />
-      <AgGridColumn field='Fish ID' />
-      <AgGridColumn field='Year' />
-      <AgGridColumn field='Field Office' />
-      <AgGridColumn field='Project' />
-      <AgGridColumn field='Segment' />
-      <AgGridColumn field='Season' />
-      <AgGridColumn field='Bend' />
-      <AgGridColumn field='Bend R/N' />
-      <AgGridColumn field='Bend River Mile' />
-      <AgGridColumn field='Panel/Hook' />
-      <AgGridColumn field='Species' />
-      <AgGridColumn field='Hatchery Origin' />
-      <AgGridColumn field='Checkby' />
-    </AgGridReact>
-  </div>
+  <>
+    <DownloadAsCSV />
+    <div className='ag-theme-balham' style={{ width: '100%', height: '600px' }}>
+      <AgGridReact rowData={[]}>
+        <AgGridColumn field='Unique ID' />
+        <AgGridColumn field='Fish ID' />
+        <AgGridColumn field='Year' />
+        <AgGridColumn field='Field Office' />
+        <AgGridColumn field='Project' />
+        <AgGridColumn field='Segment' />
+        <AgGridColumn field='Season' />
+        <AgGridColumn field='Bend' />
+        <AgGridColumn field='Bend R/N' />
+        <AgGridColumn field='Bend River Mile' />
+        <AgGridColumn field='Panel/Hook' />
+        <AgGridColumn field='Species' />
+        <AgGridColumn field='Hatchery Origin' />
+        <AgGridColumn field='Checkby' />
+      </AgGridReact>
+    </div>
+  </>
 );
 
 export default FishTable;
--- src/app-pages/data-summaries/tables/procedureTable.js
+++ src/app-pages/data-summaries/tables/procedureTable.js
@@ -1,43 +1,48 @@
 import React from 'react';
 import { AgGridReact, AgGridColumn } from 'ag-grid-react';
 
+import DownloadAsCSV from '../components/downloadAsCSV';
+
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 const ProcedureTable = () => (
-  <div className='ag-theme-balham' style={{ width: '100%', height: '600px' }}>
-    <AgGridReact rowData={[]}>
-      <AgGridColumn field='Last Updated' />
-      <AgGridColumn field='Purpose Code' />
-      <AgGridColumn field='Procedure Date' />
-      <AgGridColumn field='Procedure Start Time' />
-      <AgGridColumn field='Procedure End Time' />
-      <AgGridColumn field='Procedure By' />
-      <AgGridColumn field='Antibiotic Injection Ind' />
-      <AgGridColumn field='Photo Dorsal Ind' />
-      <AgGridColumn field='Photo Ventral Ind' />
-      <AgGridColumn field='Photo Left Ind' />
-      <AgGridColumn field='Old Radio Tag Num' />
-      <AgGridColumn field='Old Frequency ID' />
-      <AgGridColumn field='DST Serial Num' />
-      <AgGridColumn field='DST Start Date' />
-      <AgGridColumn field='DST Start Time' />
-      <AgGridColumn field='DST Reimplant ID' />
-      <AgGridColumn field='New Radio Tag Num' />
-      <AgGridColumn field='New Frequency ID' />
-      <AgGridColumn field='Sex Code' />
-      <AgGridColumn field='Fish Health Comments' />
-      <AgGridColumn field='Spawn Code' />
-      <AgGridColumn field='Eval Location Code' />
-      <AgGridColumn field='Blood Sample Ind' />
-      <AgGridColumn field='Egg Sample Ind' />
-      <AgGridColumn field='Visual Repro Status Code' />
-      <AgGridColumn field='Ultrasound Repro Status Code' />
-      <AgGridColumn field='Ultrasound Gonad Length' />
-      <AgGridColumn field='Gonad Condition' />
-      <AgGridColumn field='Expected Spawn Year' />
-    </AgGridReact>
-  </div>
+  <>
+    <DownloadAsCSV />
+    <div className='ag-theme-balham' style={{ width: '100%', height: '600px' }}>
+      <AgGridReact rowData={[]}>
+        <AgGridColumn field='Last Updated' />
+        <AgGridColumn field='Purpose Code' />
+        <AgGridColumn field='Procedure Date' />
+        <AgGridColumn field='Procedure Start Time' />
+        <AgGridColumn field='Procedure End Time' />
+        <AgGridColumn field='Procedure By' />
+        <AgGridColumn field='Antibiotic Injection Ind' />
+        <AgGridColumn field='Photo Dorsal Ind' />
+        <AgGridColumn field='Photo Ventral Ind' />
+        <AgGridColumn field='Photo Left Ind' />
+        <AgGridColumn field='Old Radio Tag Num' />
+        <AgGridColumn field='Old Frequency ID' />
+        <AgGridColumn field='DST Serial Num' />
+        <AgGridColumn field='DST Start Date' />
+        <AgGridColumn field='DST Start Time' />
+        <AgGridColumn field='DST Reimplant ID' />
+        <AgGridColumn field='New Radio Tag Num' />
+        <AgGridColumn field='New Frequency ID' />
+        <AgGridColumn field='Sex Code' />
+        <AgGridColumn field='Fish Health Comments' />
+        <AgGridColumn field='Spawn Code' />
+        <AgGridColumn field='Eval Location Code' />
+        <AgGridColumn field='Blood Sample Ind' />
+        <AgGridColumn field='Egg Sample Ind' />
+        <AgGridColumn field='Visual Repro Status Code' />
+        <AgGridColumn field='Ultrasound Repro Status Code' />
+        <AgGridColumn field='Ultrasound Gonad Length' />
+        <AgGridColumn field='Gonad Condition' />
+        <AgGridColumn field='Expected Spawn Year' />
+      </AgGridReact>
+    </div>
+  </>
 );
 
 export default ProcedureTable;
