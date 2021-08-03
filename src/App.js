42a0b546a099c1168cc216336f80c3c23f6ff6c6
KevinJJackson <kevinjjackson93@gmail.com>
2021-08-03 09:35:59 -0400

    footer-build-issues

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { connect } from 'redux-bundler-react';
 
 import Modal from 'app-components/modal';
 import NavBar from 'app-components/navigation';
-import Footer from './common/footer';
+import Footer from './common/footer/Footer';
 import PageContent from 'app-components/page-content';
 
 import './css/bootstrap/css/bootstrap.water.min.css';
