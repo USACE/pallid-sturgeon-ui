6301a7a709775af683dd9b5ec23c5c02addd4c4d
KevinJJackson <kevinjjackson93@gmail.com>
2021-08-25 14:24:47 -0400

    fix action name

--- src/app-bundles/upload-bundle.js
+++ src/app-bundles/upload-bundle.js
@@ -29,7 +29,7 @@ export default {
 
     apiPost(url, payload, (err, _body) => {
       if (!err) {
-        dispatch({ type: 'SEARCH_REPORTS_FETCH_FINISHED' });
+        dispatch({ type: 'UPLOAD_FILES_FINISHED' });
       } else {
         dispatch({ type: 'UPLOAD_FILES_ERROR', payload: err });
       }
