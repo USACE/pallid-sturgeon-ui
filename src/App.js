9bbb47620278968ae9cce3d255bfd34b1131aabd
Jazlene Rae Guevarra <jfg3xs@virginia.edu>
2023-03-23 16:23:39 -0400

    added disclaimer modal before user login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'redux-bundler-react';
 import { ToastContainer } from 'react-toastify';
 
@@ -7,20 +7,41 @@ import NavBar from 'app-components/navigation';
 import Hero from 'app-components/hero';
 import Footer from './common/footer/Footer';
 import PageContent from 'app-components/page-content';
+import LandingModal from 'common/modals/landing';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './css/bootstrap/css/bootstrap.water.min.css';
 import './css/mdi/css/materialdesignicons.min.css';
 import './css/index.scss';
 
-export default connect('selectRoute', 'selectAuth', ({ route: Route, auth }) => (
-  <>
-    <ToastContainer autoClose={3500} hideProgressBar={false} />
-    <NavBar />
-    <PageContent>
-      {auth.token ? <Route />:<Hero />}
-    </PageContent>
-    <Modal closeWithEscape />
-    <Footer />
-  </>
-));
+export default connect(
+  'doModalOpen',
+  'doModalClose',
+  'selectRoute', 
+  'selectAuth', 
+  ({ 
+    doModalOpen,
+    doModalClose,
+    route: Route, 
+    auth 
+  }) => {
+    // @TODO: Change logic for this
+    useEffect(() => {
+      if (!auth.token) {
+        doModalOpen(LandingModal);
+      } else {
+        doModalClose(LandingModal);
+      }
+    }, [auth, doModalClose, doModalOpen]);
+
+    return (
+      <>
+        <ToastContainer autoClose={3500} hideProgressBar={false} />
+        <NavBar />
+        <PageContent>
+          {auth.token ? <Route />:<Hero />}
+        </PageContent>
+        <Modal closeWithEscape />
+        <Footer />
+      </>
+    );});
