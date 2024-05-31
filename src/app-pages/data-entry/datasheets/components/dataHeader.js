f2d7defc206f7fc33fcc3dd35e48cdc098ff7a84
Jasmin <henry_jasmin@bah.com>
2024-05-31 11:56:39 -0400

    update dataHeader

--- src/app-pages/data-entry/datasheets/components/dataHeader.js
+++ src/app-pages/data-entry/datasheets/components/dataHeader.js
@@ -9,13 +9,8 @@ import '../../dataentry.scss';
 
 const DataHeader = connect(
   'selectHeaderData',
-  'selectDataEntryLastParams',
   'selectRouteParams',
-  'selectUserRole',
-  ({ doFetchHeaderData, headerData, dataEntryLastParams, routeParams, userRole, id, year }) => {
-    useEffect(() => {
-      doFetchHeaderData({ siteId: id, office: userRole?.officeCode, year: year, project: userRole?.projectCode, mrId: dataEntryLastParams?.mrId });
-    }, [id]);
+  ({ headerData, routeParams, id }) => {
 
     return (
       <Card className='mb-3'>
@@ -80,66 +75,6 @@ const DataHeader = connect(
       </Card>
     );
   }
-);
-  ({
-    headerData,
-    id,
-  }) =>  (
-    <Card className='mb-3'>
-      <Card.Body>
-        <Row>
-          <div className='col-md-12 col-xs-4'>
-            <Row className='border-bottom'>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Site ID:</b>
-                {id || '--'}
-              </div>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Year:</b>
-                {headerData?.[0]?.year || '--'}
-              </div>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Field Office:</b>
-                {headerData?.[0]?.fieldoffice || '--'}
-              </div>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Project:</b>
-                {headerData?.[0]?.project || '--'}
-              </div>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Segment:</b>
-                {headerData?.[0]?.segment || '--'}
-              </div>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Season:</b>
-                {headerData?.[0]?.season || '--'}
-              </div>
-            </Row>
-          </div>
-          <div className='col-md-12 col-xs-4 mt-2'>
-            <Row>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Sample Unit Type:</b>
-                {headerData?.[0]?.sampleUnitType || '--'}
-              </div>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Sample Unit:</b>
-                {headerData?.[0]?.bend || '--'}
-              </div>
-              <div className='col-sm-2'>
-                <b className='mr-2'>R/N:</b>
-                {headerData?.[0]?.bendrn || '--'}
-              </div>
-              <div className='col-sm-2'>
-                <b className='mr-2'>Bend River Mile:</b>
-                {headerData?.[0]?.bendrivermile || '--'}
-              </div>
-            </Row>
-          </div>
-        </Row>
-      </Card.Body>
-    </Card>
-  )
 );
 
 export default DataHeader;
