ac7b34aca2f92e91165bef69aef67ad1de5c7f74
Jazlene Guevarra <guevarra_jazlenerae@bah.com>
2026-02-02 14:49:29 -0500

    create lookup-bundle for lookup table data

---
+++ src/app-bundles/lookup-bundle.jsx
@@ -0,0 +1,48 @@
+import { ApiStatuses } from '@src/utils/enums';
+
+const rootUrl = '/psapi/Lookup/';
+
+const lookupBundle = {
+  name: 'lookup',
+
+  getReducer: () => {
+    const initialState = {
+      bendSelections: [],
+      gearCodes: [],
+      filteredGearCodes: [],
+      gearTypes: [],
+      macros: [],
+      mesos: [],
+      macroMesos: [],
+      u7: [],
+    };
+
+    return (state = initialState, { type, payload }) => {
+      switch (type) {
+        case 'UPDATE_ALL_LOOKUP':
+          return {
+            ...state,
+            ...payload,
+          };
+        default:
+          return state;
+      }
+    };
+  },
+
+  selectLookupData: (state) => state.lookup,
+
+  doGetAllLookupData:
+    () =>
+    ({ dispatch, apiGet }) => {
+      const url = `${rootUrl}getAllLookups`;
+      apiGet(url, (err, body) => {
+        if (!err && body?.status === ApiStatuses.Success) {
+          dispatch({ type: 'UPDATE_ALL_LOOKUP', payload: body?.data });
+        } else {
+          dispatch({ type: 'LOOKUP_FETCH_ERROR', payload: err });
+        }
+      });
+    },
+};
+export default lookupBundle;
