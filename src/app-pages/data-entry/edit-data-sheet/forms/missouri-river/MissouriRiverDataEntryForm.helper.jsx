cd0e47334e4ea42e198f159b54e41ce0c5260b5e
Jazlene Guevarra <guevarra_jazlenerae@bah.com>
2026-03-04 13:45:41 -0500

    remove helper file

--- src/app-pages/data-entry/edit-data-sheet/forms/missouri-river/MissouriRiverDataEntryForm.helper.jsx
+++
@@ -1,9 +0,0 @@
-export const missouriRiverTooltip = {
-  startTimeTooltip:
-    'Both Velocity (bot) 1 and Velocity (0.8 or 0.5) 1 must be filled out before setting the start time when using a larval drift net gear.',
-  distanceTooltip: 'Gear Code must have a value in order to input Distance',
-};
-
-export const u6Options = [{ value: 'MNCF' }, { value: 'NSTS' }];
-
-export const siltSandGravelReadOnly = ['SD', 'NE', 'MT', 'MR'];
