c934a2a59061415ff68603c7ae281da3ee3e9942
jhenr098 <henry_jasmin@bah.com>
2026-04-01 12:48:31 -0400

    added more validations, added buttons, fixed exist

--- src/app-pages/data-entry/datasheets/tables/telemetry/TelemetryDataEntry.jsx
+++ src/app-pages/data-entry/datasheets/tables/telemetry/TelemetryDataEntry.jsx
@@ -7,12 +7,35 @@ import _isEqual from 'lodash/isEqual';
 
 import DataEntryTable from '@src/app-components/table/data-entry-table/DataEntryTable';
 import { TableCell } from '@src/app-components/table/table-cell-components/TableCell';
+import { Button, Alert, Grid } from '@trussworks/react-uswds';
+import classNames from 'classnames';
 
 import { frequencyIdOptions } from '../../../edit-data-sheet/forms/_shared/selectHelper';
-import { telemetryDataEntrySchema } from './TelemetryDataEntry.validation';
+import {
+  telemetryDataEntrySchema,
+  getBaseDefaultValues,
+  getTelemetryDefaultValues,
+} from './TelemetryDataEntry.validation';
 
 import '@pages/data-summaries/data-summary.scss';
 import '@pages/data-entry/dataentry.scss';
+import { update } from 'lodash';
+// import { createDropdownOptions } from '@src/app-pages/data-entry/helpers';
+
+const saveBtnClasses = classNames('button-small', 'text-normal', 'save-btn');
+
+const createDropdownOptions = (data) => {
+  if (!data) return [];
+
+  return data.map((item) => {
+    const { code, description } = item;
+
+    return {
+      value: code,
+      text: description,
+    };
+  });
+};
 
 const TelemetryDataEntry = connect(
   'doModalOpen',
@@ -22,6 +45,8 @@ const TelemetryDataEntry = connect(
   'selectDataEntryLastParams',
   'selectUserRole',
   'selectBaseData',
+  'selectDataEntryData',
+  'selectLookupData',
   ({
     doModalOpen,
     doSaveTelemetryDataEntry,
@@ -30,23 +55,36 @@ const TelemetryDataEntry = connect(
     dataEntryLastParams,
     userRole,
     baseData,
+    dataEntryData,
+    lookupData,
   }) => {
+    const { frequencyId, spawnBehavior, macros, mesos, positionConfidence } = lookupData;
     const { items } = dataEntryTelemetryData;
 
     const rowData = items?.map((item) => ({ ...item, bendRiverMile: baseData?.bendRiverMile }));
     const [tableKey, setTableKey] = useState(0);
     const [tableErrors, setTableErrors] = useState();
-    const [data, setData] = useState(rowData);
+    const [data, setData] = useState([]);
     const [tableIsDirty, setTableIsDirty] = useState(false);
     const prevTableDataRef = useRef([]);
     const columnHelper = createColumnHelper();
 
     const defaultValues = { seId: dataEntryLastParams.seId };
 
+    useEffect(() => {
+      if (items) {
+        const mapped = items.map((item) => ({
+          ...item,
+          bendRiverMile: baseData?.bendRiverMile,
+        }));
+        setData(mapped);
+      }
+    }, [items, baseData]);
+
     const methods = useForm({
       resolver: yupResolver(telemetryDataEntrySchema),
       mode: 'onBlur',
-      defaultValues: defaultValues,
+      defaultValues: getTelemetryDefaultValues({ baseData: baseData, dataEntryData: dataEntryTelemetryData }),
     });
     const {
       formState: { errors, dirtyFields },
@@ -70,11 +108,32 @@ const TelemetryDataEntry = connect(
           cell: ({ cell }) => <span>{cell.getValue()}</span>,
           size: 150,
         }),
+        columnHelper.accessor('copy', {
+          header: 'Copy Data',
+          cell: ({ row }) => (
+            <Button
+              className={saveBtnClasses}
+              onClick={() => {
+                if (row.index === 0) return;
+
+                const prevRow = data[row.index - 1];
+
+                handleUpdateData(row.index, null, {
+                  radioTagNum: prevRow.radioTagNum,
+                  frequencyId: prevRow.frequencyId,
+                });
+              }}
+              type='button'
+            >
+              Copy Data
+            </Button>
+          ),
+        }),
         columnHelper.accessor('bend', {
           header: 'Bend',
           cell: TableCell,
           size: 200,
-          meta: { type: 'text' },
+          meta: { readOnly: true },
         }),
         columnHelper.accessor('bendRiverMile', {
           header: 'Bend River Mile',
@@ -94,8 +153,28 @@ const TelemetryDataEntry = connect(
           meta: {
             type: 'select',
             required: true,
-            options: frequencyIdOptions,
+            options: createDropdownOptions(frequencyId),
           },
+        }),
+        columnHelper.accessor('captureButton', {
+          header: 'Capture Button',
+          cell: ({ row }) => (
+            <Button
+              className={saveBtnClasses}
+              onClick={() => {
+                const computedValues = {
+                  captureDate: 'captureDate',
+                  captureLatitude: 'captureLatitude',
+                  captureLongitude: 'captureLongitude',
+                };
+                handleUpdateData(row.index, computedValues);
+              }}
+              type='button'
+            >
+              Capture Button
+            </Button>
+          ),
+          size: 200,
         }),
         columnHelper.accessor('captureDate', {
           header: 'Capture Time',
@@ -120,27 +199,49 @@ const TelemetryDataEntry = connect(
           header: 'Position Confidence',
           cell: TableCell,
           size: 200,
-          meta: { type: 'number', required: true },
+          meta: {
+            type: 'select',
+            required: true,
+            options: createDropdownOptions(positionConfidence),
+          },
+        }),
+        columnHelper.accessor('spawnBehavior', {
+          header: 'Spawn Behavior',
+          cell: TableCell,
+          size: 200,
+          meta: {
+            type: 'select',
+            required: true,
+            options: createDropdownOptions(spawnBehavior),
+          },
         }),
         columnHelper.accessor('mesoId', {
           // select?
           header: 'Meso',
           cell: TableCell,
           size: 200,
-          meta: { type: 'text' },
-        }),
-        columnHelper.accessor('depth', {
-          header: 'Depth',
-          cell: TableCell,
-          size: 200,
-          meta: { type: 'number' },
+          meta: {
+            type: 'select',
+            required: true,
+            options: createDropdownOptions(mesos),
+          },
         }),
         columnHelper.accessor('macroId', {
           // select?
           header: 'Macro',
           cell: TableCell,
           size: 200,
-          meta: { type: 'text' },
+          meta: {
+            type: 'select',
+            required: true,
+            options: createDropdownOptions(macros),
+          },
+        }),
+        columnHelper.accessor('depth', {
+          header: 'Depth',
+          cell: TableCell,
+          size: 200,
+          meta: { type: 'number' },
         }),
         columnHelper.accessor('temp', {
           header: 'Temp',
@@ -211,6 +312,16 @@ const TelemetryDataEntry = connect(
       [columnHelper]
     );
 
+    const handleAddRow = () => {
+      // Add default values here
+      const base = getBaseDefaultValues({ baseData });
+      const newRowData = {
+        ...base,
+        countF: 1,
+      };
+      setData((prev) => (prev ? [...prev, newRowData] : [newRowData]));
+    };
+
     const handleAddMultipleRows = (rows) => {
       // Handle any data mapping or formatting here
       setData((oldData) => {
@@ -231,21 +342,50 @@ const TelemetryDataEntry = connect(
     );
 
     const handleUpdateData = useCallback(
-      (rowIndex, columnId, updatedValue) => {
+      (rowIndex, columnId, value) => {
         setData((oldData) => {
-          const newData = oldData ? [...oldData] : null;
-          if (newData && newData[rowIndex]) {
-            // Update properties
+          if (!oldData) return [];
+
+          const newData = [...oldData];
+          if (!newData[rowIndex]) return oldData;
+
+          if (typeof value === 'object' && columnId === null) {
             newData[rowIndex] = {
               ...newData[rowIndex],
-              [columnId]: updatedValue,
+              ...value,
             };
-            return newData;
+          } else {
+            newData[rowIndex] = {
+              ...newData[rowIndex],
+              [columnId]: value,
+            };
           }
+
+          // if (newData && newData[rowIndex]) {
+          //   // Update properties
+          //   newData[rowIndex] = {
+          //     ...newData[rowIndex],
+          //     [columnId]: updatedValue,
+          //   };
+          return newData;
         });
       },
-      [setData]
+      // [setData]
+      []
     );
+
+    const handleSubmitAll = async () => {
+      try {
+        for (let i = 0; i < data.length; i++) {
+          await telemetryDataEntrySchema.validate(data, { abortEarly: false });
+        }
+
+        await doSaveTelemetryDataEntry(data);
+        console.log('Submitted:', data);
+      } catch (err) {
+        console.error('Validation failed:', err);
+      }
+    };
 
     useEffect(() => {
       const tableDataChanged = !_isEqual(data, prevTableDataRef.current);
@@ -257,21 +397,26 @@ const TelemetryDataEntry = connect(
 
     return (
       <FormProvider {...methods}>
-        <DataEntryTable
-          addRow={() => setData((prev) => (prev ? [...prev, {}] : [{}]))}
-          columns={tableColumns}
-          data={data}
-          initialTableState={{}}
-          key={tableKey}
-          placeholderClick={() => setData((prev) => (prev ? [...prev, {}] : [{}]))}
-          placeholderText='No Telemetry Data found.'
-          removeMultipleRows={handleRemoveMultipleRows}
-          addMultipleRows={handleAddMultipleRows}
-          rowErrorCallback={setTableErrors}
-          tableVersion='TelemetryTable'
-          updateSourceData={handleUpdateData}
-          validationSchema={telemetryDataEntrySchema}
-        />
+        <>
+          <DataEntryTable
+            addRow={handleAddRow}
+            columns={tableColumns}
+            data={data}
+            initialTableState={{}}
+            key={tableKey}
+            placeholderClick={handleAddRow}
+            placeholderText='No Telemetry Data found.'
+            removeMultipleRows={handleRemoveMultipleRows}
+            addMultipleRows={handleAddMultipleRows}
+            rowErrorCallback={setTableErrors}
+            tableVersion='TelemetryTable'
+            updateSourceData={handleUpdateData}
+            validationSchema={telemetryDataEntrySchema}
+          />
+          <Button className={saveBtnClasses} onClick={handleSubmitAll} type='button'>
+            Submit
+          </Button>
+        </>
       </FormProvider>
     );
   }
