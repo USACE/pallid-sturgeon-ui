89138a2d34b3358abb737b9072fa21d3a821fb75
Jazlene Rae Guevarra <43012576+jguevarra@users.noreply.github.com>
2026-04-16 16:02:24 -0400

    feat/CWBI-28141-supplink (#162)

---
+++ src/app-components/table/table-cell-components/fish/FishLinkTableCell.jsx
@@ -0,0 +1,37 @@
+import { connect } from 'redux-bundler-react';
+import Icon from '@components/icon/icon';
+import { mdiEye, mdiPlusBox } from '@mdi/js';
+import { Button } from '@trussworks/react-uswds';
+
+const getModalProps = ({ row, isEdit }) => ({
+  data: row.original,
+  edit: isEdit,
+  rowIndex: row.index,
+});
+
+const FishLinkTableCell = connect('doModalOpen', ({ doModalOpen, modalComponent, row }) => {
+  const isEdit = !!(row.original.fFid || row.original.fid);
+  const modalProps = getModalProps({ row, isEdit });
+
+  const handleButtonClick = () => doModalOpen(modalComponent, modalProps);
+
+  return (
+    <div className='button-container'>
+      <Button
+        className='button-small text-normal'
+        title='Add Supplemental & Procedure Data Entry'
+        onClick={() => handleButtonClick()}
+        type='button'
+      >
+        <Icon
+          aria-label={`${isEdit ? 'View' : 'Add'} Supplemental & Procedure Data Entry`}
+          focusable={false}
+          path={isEdit ? mdiEye : mdiPlusBox}
+          size={'15px'}
+        />
+      </Button>
+    </div>
+  );
+});
+
+export default FishLinkTableCell;
