90e3f1b1c7abd8c80e95454f6be873d4eb2f3d53
Jazlene Guevarra <guevarra_jazlenerae@bah.com>
2026-08-19 16:29:17 -0400

    Have data header pull offline and online data according to appropriate network status

--- src/app-pages/sites-list/sites-list-table/siteIdCellRenderer.jsx
+++ src/app-pages/sites-list/sites-list-table/siteIdCellRenderer.jsx
@@ -21,27 +21,44 @@ const SiteIdCellRenderer = connect(
     doFetchSites,
     doUpdateUrl,
     doModalOpen,
-    edit,
+    edit: viewSiteForm,
     data,
     value,
   }) => {
-    const siteId = data?.siteId ?? data?.site_id ?? data?.serverId;
-    const siteFid = data?.siteFid ?? data?.site_fid;
+    // Pull Site Unique Identifiers
+    const siteId = data?.siteId;
+    const siteFid = data?.siteFid;
     const siteRouteKey = Number(siteId) > 0 ? String(siteId) : (data?.siteRouteKey ?? siteFid);
+    // Check network status (offline/online)
+    const isOnline = navigator.onLine;
+
+    const handleCallback = () => {
+      doUpdateUrl(`/sites-list/${data?.siteId}`);
+    };
 
     const handleClick = () => {
-      if (edit) {
-        if (navigator.onLine) {
+      // Online Status Conditions
+      if (isOnline) {
+        if (viewSiteForm) {
+          // Load all Domain Lookup Table Data
           doDomainFieldOfficesFetch();
           doDomainBendRnFetch();
           doFetchUsers();
           doDomainSeasonsFetch();
           doDomainSegmentsFetch({ office: data?.fieldoffice, project: data?.projectId });
+          // Open Sites Form Modal
+          doModalOpen(SitesFormModal, { edit: true, data: data });
+        } else {
+          // Navigate to Sites Datasheet Page and Fetch Online Sites by Selected Site Unique Identifier
+          // This populates Base Data for Data Header Component
+          doFetchSites({ siteId: data?.siteId }, handleCallback());
         }
-        doModalOpen(SitesFormModal, { edit: true, data: data });
-        return;
+      } else {
+        // Offline Status Conditions
+        viewSiteForm
+          ? doModalOpen(SitesFormModal, { edit: true, data: data })
+          : doUpdateUrl(`/sites-list/${siteRouteKey}`);
       }
-      doUpdateUrl(`/sites-list/${siteRouteKey}`);
     };
 
     return <Button size='small' variant='link' className='p-0 mb-1' text={value} handleClick={() => handleClick()} />;
